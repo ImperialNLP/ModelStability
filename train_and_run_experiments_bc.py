8084b4b70b946e1f09161d1795aebe5f3be00c2f
Rishabh Jain <wakeuprj@gmail.com>
2019-05-21 23:30:27 +0100

    Added seeds.

--- train_and_run_experiments_bc.py
+++ train_and_run_experiments_bc.py
@@ -1,4 +1,9 @@
 import argparse
+import numpy as np
+import os
+import random
+import torch
+
 parser = argparse.ArgumentParser(description='Run experiments on a dataset')
 parser.add_argument('--dataset', type=str, required=True)
 parser.add_argument("--data_dir", type=str, required=True)
@@ -9,6 +14,12 @@ parser.add_argument('--attention', type=str, choices=['tanh', 'dot', 'all'], req
 args, extras = parser.parse_known_args()
 args.extras = extras
 
+pseudo_random_seed = 21
+os.environ['PYTHONHASHSEED'] = str(pseudo_random_seed)
+np.random.seed(pseudo_random_seed)
+random.seed(pseudo_random_seed)
+torch.manual_seed(pseudo_random_seed)
+
 from Transparency.Trainers.DatasetBC import *
 from Transparency.ExperimentsBC import *
 
@@ -16,16 +27,16 @@ dataset = datasets[args.dataset](args)
 
 if args.output_dir is not None :
     dataset.output_dir = args.output_dir
-    
+
 encoders = ['cnn', 'lstm', 'average'] if args.encoder == 'all' else [args.encoder]
 
 if args.attention in ['tanh', 'all'] :
     train_dataset_on_encoders(dataset, encoders)
-#    generate_graphs_on_encoders(dataset, encoders)
+    # generate_graphs_on_encoders(dataset, encoders)
 if args.attention in ['dot', 'all'] :
     encoders = [e + '_dot' for e in encoders]
     train_dataset_on_encoders(dataset, encoders)
-#    generate_graphs_on_encoders(dataset, encoders)
+    # generate_graphs_on_encoders(dataset, encoders)
 
 
 
