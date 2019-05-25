5c5f1e795688acdbde36cf154238114bf22d1c05
Rishabh Jain <wakeuprj@gmail.com>
2019-05-25 15:54:27 +0100

    add script to run/evaluate model given path.

--- ExperimentsBC.py
+++ ExperimentsBC.py
@@ -3,7 +3,7 @@ from Transparency.Trainers.PlottingBC import generate_graphs, plot_adversarial_e
 from Transparency.configurations import configurations
 from Transparency.Trainers.TrainerBC import Trainer, Evaluator
 from Transparency.model.LR import LR
-            
+
 def train_dataset(dataset, config='lstm') :
     try :
         config = configurations[config](dataset)
@@ -36,14 +36,14 @@ def train_dataset_on_encoders(dataset, encoders) :
     for e in encoders :
         train_dataset(dataset, e)
         run_experiments_on_latest_model(dataset, e)
-        
+
 def generate_graphs_on_encoders(dataset, encoders) :
     for e in encoders :
         generate_graphs_on_latest_model(dataset, e)
 
 def train_lr_on_dataset(dataset) :
     config = {
-        'vocab' : dataset.vec, 
+        'vocab' : dataset.vec,
         'stop_words' : True,
         'type' : dataset.trainer_type,
         'exp_name' : dataset.name
@@ -67,6 +67,13 @@ def run_evaluator_on_latest_model(dataset, config='lstm') :
     _ = evaluator.evaluate(dataset.test_data, save_results=True)
     return evaluator
 
+def run_evaluator_on_specific_model(dataset, model_path, config='lstm'):
+    config = configurations[config](dataset)
+    evaluator = Evaluator(dataset, model_path, _type=dataset.trainer_type)
+    _ = evaluator.evaluate(dataset.test_data, save_results=True)
+    return evaluator
+
+
 def run_experiments_on_latest_model(dataset, config='lstm', force_run=True) :
     try :
         evaluator = run_evaluator_on_latest_model(dataset, config)
@@ -78,7 +85,7 @@ def run_experiments_on_latest_model(dataset, config='lstm', force_run=True) :
     except Exception as e:
         print(e)
         return
-        
+
 def generate_graphs_on_latest_model(dataset, config='lstm') :
     config = configurations[config](dataset)
     latest_model = get_latest_model(os.path.join(config['training']['basepath'], config['training']['exp_dirname']))
@@ -124,8 +131,8 @@ def get_results(path) :
         raise LookupError("No Latest Model ... ")
 
 names = {
-    'vanilla_lstm':'LSTM', 
-    'lstm':'LSTM + Additive Attention', 
+    'vanilla_lstm':'LSTM',
+    'lstm':'LSTM + Additive Attention',
     'logodds_lstm':'LSTM + Log Odds Attention',
     'lr' : 'LR + BoW',
     'logodds_lstm_post' : 'LSTM + Additive Attention (Log Odds at Test)'
@@ -141,11 +148,11 @@ def push_all_models(dataset, keys) :
 
     path = os.path.join('outputs', dataset.name, 'LR+TFIDF')
     evals = get_results(path)
-    model_evals[names['lr']] = {keys[k]:evals[k] for k in keys} 
+    model_evals[names['lr']] = {keys[k]:evals[k] for k in keys}
 
     path = os.path.join('outputs', dataset.name, 'lstm+tanh+logodds(posthoc)')
     evals = get_results(path)
-    model_evals[names['logodds_lstm_post']] = {keys[k]:evals[k] for k in keys} 
+    model_evals[names['logodds_lstm_post']] = {keys[k]:evals[k] for k in keys}
 
     df = pd.DataFrame(model_evals).transpose()
     df['Model'] = df.index
---
+++ eval_model.py
@@ -0,0 +1,43 @@
+import argparse
+import numpy as np
+import os
+import random
+import torch
+
+parser = argparse.ArgumentParser(description='Run experiments on a dataset')
+parser.add_argument('--dataset', type=str, required=True)
+parser.add_argument("--data_dir", type=str, required=True)
+parser.add_argument("--output_dir", type=str)
+parser.add_argument('--encoder', type=str,
+                    choices=['cnn', 'lstm', 'average', 'all'], required=True)
+parser.add_argument('--attention', type=str, choices=['tanh', 'dot', 'all'],
+                    required=True)
+parser.add_argument('--path', type=str, required=True)
+
+args, extras = parser.parse_known_args()
+args.extras = extras
+
+pseudo_random_seed = 21
+os.environ['PYTHONHASHSEED'] = str(pseudo_random_seed)
+np.random.seed(pseudo_random_seed)
+random.seed(pseudo_random_seed)
+torch.manual_seed(pseudo_random_seed)
+
+from Transparency.Trainers.DatasetBC import *
+from Transparency.ExperimentsBC import *
+
+dataset = datasets[args.dataset](args)
+
+if args.output_dir is not None:
+    dataset.output_dir = args.output_dir
+
+encoders = ['cnn', 'lstm', 'average'] if args.encoder == 'all' else [
+    args.encoder]
+
+if args.attention in ['tanh', 'all']:
+    # generate_graphs_on_encoders(dataset, encoders)
+    run_evaluator_on_specific_model(dataset, args.path, args.encoder)
+if args.attention in ['dot', 'all']:
+    encoders = [e + '_dot' for e in encoders]
+    run_evaluator_on_specific_model(dataset, args.path, args.encoder)
+    # generate_graphs_on_encoders(dataset, encoders)
