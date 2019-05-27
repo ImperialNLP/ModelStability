e7b151d26d83a37d31847b200b90ce4b27cc45f1
Rishabh Jain <wakeuprj@gmail.com>
2019-05-27 17:05:53 +0100

    dist corr when swa'ing

--- model/Binary_Classification.py
+++ model/Binary_Classification.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from allennlp.common import Params
+from dcor import distance_correlation
 from sklearn.utils import shuffle
 from tqdm import tqdm
 from torchcontrib.optim import SWA
@@ -92,8 +93,7 @@ class Model() :
         pos_weight = configuration['training'].get('pos_weight', [1.0]*self.decoder.output_size)
         self.pos_weight = torch.Tensor(pos_weight).to(device)
         self.criterion = nn.BCEWithLogitsLoss(reduction='none').to(device)
-        # self.swa_settings = configuration['training']['swa']
-        self.swa_settings = [0,0,0,0]
+        self.perf_swa = 'swa' in configuration['training'] and configuration['training']['swa']
 
         import time
         dirname = configuration['training']['exp_dirname']
@@ -101,13 +101,14 @@ class Model() :
         self.time_str = time.ctime().replace(' ', '_')
         self.dirname = os.path.join(basepath, dirname, self.time_str)
 
-        if self.swa_settings[0]:
+        if self.perf_swa:
+            self.swa_settings = configuration['training']['swa']
             # self.attn_optim = SWA(self.attn_optim, swa_start=3, swa_freq=1, swa_lr=0.05)
             # self.decoder_optim = SWA(self.decoder_optim, swa_start=3, swa_freq=1, swa_lr=0.05)
             # self.encoder_optim = SWA(self.encoder_optim, swa_start=3, swa_freq=1, swa_lr=0.05)
             self.swa_all_optim = SWA(self.all_optim,
-                                     swa_start=self.swa_settings[1],
-                                     swa_freq=self.swa_settings[2],
+                                     # swa_start=self.swa_settings[1],
+                                     # swa_freq=self.swa_settings[2],
                                      swa_lr=self.swa_settings[3])
 
 
@@ -158,6 +159,20 @@ class Model() :
 
             if train :
                 if self.swa_settings[0]:
+                    if self.swa_all_optim.param_groups[0]['step_counter'] > \
+                        self.swa_settings[1] \
+                        and self.swa_all_optim.param_groups[0]['step_counter'] % \
+                        self.swa_settings[2] == 0:
+
+                        p = self.swa_all_optim.param_groups[0]['params'][-2]
+                        param_state = self.swa_all_optim.state[p]
+                        if 'swa_buffer' in param_state:
+                            buf = np.squeeze(param_state['swa_buffer'].cpu().numpy())
+                            cur_state = np.squeeze(p.data.cpu().numpy())
+                            d_correlation = distance_correlation(buf, cur_state)
+                            if d_correlation > 0.9:
+                                self.swa_all_optim.update_swa()
+
                     self.swa_all_optim.zero_grad()
                     loss.backward()
                     self.swa_all_optim.step()
