dc24fa4bd386bcedc65cbf02eb886b2847e17994
Rishabh Jain <wakeuprj@gmail.com>
2019-05-27 19:25:57 +0100

    clean parser stuff.

--- train_and_run_stability_test_bc.py
+++ train_and_run_stability_test_bc.py
@@ -24,43 +24,44 @@ def get_parser():
     parser.add_argument("--temp", type=float, default=1)
     return parser
 
+if __name__ == "__main__":
 
-args, extras = get_parser().parse_known_args()
-args.extras = extras
+    args, extras = get_parser().parse_known_args()
+    args.extras = extras
 
-from Transparency.Trainers.DatasetBC import *
-from Transparency.ExperimentsBC import *
+    from Transparency.Trainers.DatasetBC import *
+    from Transparency.ExperimentsBC import *
 
-dataset = datasets[args.dataset](args)
+    dataset = datasets[args.dataset](args)
 
-if args.output_dir is not None:
-    dataset.output_dir = args.output_dir
+    if args.output_dir is not None:
+        dataset.output_dir = args.output_dir
 
-encoders = ['cnn', 'lstm', 'average'] if args.encoder == 'all' else [
-    args.encoder]
+    encoders = ['cnn', 'lstm', 'average'] if args.encoder == 'all' else [
+        args.encoder]
 
-seeds = eval(args.seeds)
+    seeds = eval(args.seeds)
 
-all_outputs = []
-for pseudo_random_seed in seeds:
-    os.environ['PYTHONHASHSEED'] = str(pseudo_random_seed)
-    np.random.seed(pseudo_random_seed)
-    random.seed(pseudo_random_seed)
-    torch.manual_seed(pseudo_random_seed)
-    preds, atns = [], []
-    if args.attention in ['tanh', 'all']:
-        preds, atns = train_dataset_and_get_atn_map(dataset, encoders)
-        # generate_graphs_on_encoders(dataset, encoders)
-    if args.attention in ['dot', 'all']:
-        encoders = [e + '_dot' for e in encoders]
-        preds, atns = train_dataset_and_get_atn_map(dataset, encoders)
-        # generate_graphs_on_encoders(dataset, encoders)
-    all_outputs.append((preds, atns))
+    all_outputs = []
+    for pseudo_random_seed in seeds:
+        os.environ['PYTHONHASHSEED'] = str(pseudo_random_seed)
+        np.random.seed(pseudo_random_seed)
+        random.seed(pseudo_random_seed)
+        torch.manual_seed(pseudo_random_seed)
+        preds, atns = [], []
+        if args.attention in ['tanh', 'all']:
+            preds, atns = train_dataset_and_get_atn_map(dataset, encoders)
+            # generate_graphs_on_encoders(dataset, encoders)
+        if args.attention in ['dot', 'all']:
+            encoders = [e + '_dot' for e in encoders]
+            preds, atns = train_dataset_and_get_atn_map(dataset, encoders)
+            # generate_graphs_on_encoders(dataset, encoders)
+        all_outputs.append((preds, atns))
 
-swa_settings = eval(args.swa)
-file_name = "stability-outputs-" + args.swa + args.seeds + str(
-    args.attention) + str(
-    args.dataset) + str(args.encoder) + str(args.temp) + ".pkl"
-pkl_file = open(file_name, 'wb')
-pickle.dump(all_outputs, pkl_file)
-pkl_file.close()
+    swa_settings = eval(args.swa)
+    file_name = "stability-outputs-" + args.swa + args.seeds + str(
+        args.attention) + str(
+        args.dataset) + str(args.encoder) + str(args.temp) + ".pkl"
+    pkl_file = open(file_name, 'wb')
+    pickle.dump(all_outputs, pkl_file)
+    pkl_file.close()
--- train_and_temp_scale.py
+++ train_and_temp_scale.py
@@ -7,30 +7,31 @@ from train_and_run_stability_test_bc import get_parser
 sys.path.append('/data/rishabh/')
 sys.path.append('/Users/apple/MEngProject/')
 
-args, extras = get_parser().parse_known_args()
-args.extras = extras
+if __name__ == "__main__":
+    args, extras = get_parser().parse_known_args()
+    args.extras = extras
 
-from Transparency.Trainers.DatasetBC import *
-from Transparency.ExperimentsBC import *
+    from Transparency.Trainers.DatasetBC import *
+    from Transparency.ExperimentsBC import *
 
-dataset = datasets[args.dataset](args)
+    dataset = datasets[args.dataset](args)
 
-if args.output_dir is not None:
-    dataset.output_dir = args.output_dir
+    if args.output_dir is not None:
+        dataset.output_dir = args.output_dir
 
-encoders = ['cnn', 'lstm', 'average'] if args.encoder == 'all' else [
-    args.encoder]
+    encoders = ['cnn', 'lstm', 'average'] if args.encoder == 'all' else [
+        args.encoder]
 
-seeds = eval(args.seeds)
+    seeds = eval(args.seeds)
 
-for pseudo_random_seed in seeds:
-    os.environ['PYTHONHASHSEED'] = str(pseudo_random_seed)
-    np.random.seed(pseudo_random_seed)
-    random.seed(pseudo_random_seed)
-    torch.manual_seed(pseudo_random_seed)
-    preds, atns = [], []
-    if args.attention in ['tanh', 'all']:
-        train_dataset_and_temp_scale(dataset, encoders)
-    if args.attention in ['dot', 'all']:
-        encoders = [e + '_dot' for e in encoders]
-        train_dataset_and_temp_scale(dataset, encoders)
+    for pseudo_random_seed in seeds:
+        os.environ['PYTHONHASHSEED'] = str(pseudo_random_seed)
+        np.random.seed(pseudo_random_seed)
+        random.seed(pseudo_random_seed)
+        torch.manual_seed(pseudo_random_seed)
+        preds, atns = [], []
+        if args.attention in ['tanh', 'all']:
+            train_dataset_and_temp_scale(dataset, encoders)
+        if args.attention in ['dot', 'all']:
+            encoders = [e + '_dot' for e in encoders]
+            train_dataset_and_temp_scale(dataset, encoders)
