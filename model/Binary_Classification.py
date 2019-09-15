0c5261b145eb013aa2d826d575d9ce3d4a3fb7f1
Rishabh Jain <wakeuprj@gmail.com>
2019-09-15 11:54:57 +0100

    minor

--- model/Binary_Classification.py
+++ model/Binary_Classification.py
@@ -261,6 +261,7 @@ class Model() :
             outputs.append(pred)
 
         ret_val = [[output_i[0], 1-output_i[0]] for output_i in outputs]
+        import ipdb; ipdb.set_trace()
         ret_val = np.array(ret_val)
 
         return ret_val
